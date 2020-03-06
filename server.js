538270d2633ccdde615238958ce2b990c1964fbb
Calvin Cao <calvinknockout@gmail.com>
2020-03-06 18:38:45 -0500

    server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const morgan = require('morgan');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(routes);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/bodybuilding',
+  { useNewUrlParser: true }
+);
+
+app.use(morgan('dev'));
+
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
