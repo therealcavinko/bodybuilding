f2969751aeda4f592b086e1afee298f353f1483a
Calvin Cao <calvinknockout@gmail.com>
2020-03-13 15:46:13 -0400

    app running

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,9 @@ import NavBar from './components/NavBar';
 import DashBoard from './pages/DashBoard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import WaterPage from './pages/WaterPage';
-import NutritionPage from './pages/NutritionPage';
 import ExercisePage from './pages/ExercisePage';
-import WeightPage from './pages/WeightPage';
-import MoreInfoPage from './pages/MoreInfoPage';
-
 import './App.css';
-
+//--------------------------------------------------------
 const App = () => (
   <Router>
     <div>
@@ -20,14 +15,10 @@ const App = () => (
         <Route exact path="/" component={DashBoard} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
-        <Route exact path="/water" component={WaterPage} />
-        <Route exact path="/nutrition" component={NutritionPage} />
         <Route exact path="/exercise" component={ExercisePage} />
-        <Route exact path="/weight" component={WeightPage} />
-        <Route exact path="/Info" component={MoreInfoPage} />
       </Switch>
     </div>
   </Router>
 );
-
+//-------------------------------------------------------
 export default App;
